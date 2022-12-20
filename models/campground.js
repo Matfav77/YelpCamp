5048846fd063777157134ebc917a05fa928246c7
Mathieu Favre <matfav@hotmail.fr>
2022-12-20 13:58:09 +0100

    Aded review model, updated campground model to link it to reviews

--- models/campground.js
+++ models/campground.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const { Schema } = mongoose;
 
 const campgroundSchema = new Schema({
     title: { type: String, required: true },
     image: { type: String, required: true },
     price: { type: Number, required: true },
     description: { type: String, required: true },
-    location: { type: String, required: true }
+    location: { type: String, required: true },
+    reviews: [{
+        type: Schema.Types.ObjectId,
+        ref: "Review"
+    }]
 });
 
 module.exports = mongoose.model("Campground", campgroundSchema);
