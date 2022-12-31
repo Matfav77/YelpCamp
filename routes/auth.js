275ff406a214affb03a8527c4e0a36646bfcf130
Mathieu Favre <matfav@hotmail.fr>
2022-12-31 11:57:39 +0100

    Added login routes and login verification via passport, no tracking of logins yet

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,32 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
 const catchAsync = require("../utils/catchAsync")
 const User = require("../models/user");
 
-router.get("/register", (req, res) => {
-    res.render("auth/register");
+router.get("/signup", (req, res) => {
+    res.render("auth/signup");
 })
 
-router.post("/register", catchAsync(async (req, res) => {
-    const { email, username, password } = req.body;
-    const newUser = new User({ email, username });
-    const registeredUser = await User.register(newUser, password);
-    req.flash("success", "Welcome to Yelpcamp");
-    res.redirect("/campgrounds");
+router.post("/signup", catchAsync(async (req, res) => {
+    try {
+        const { email, username, password } = req.body;
+        const newUser = new User({ email, username });
+        const registeredUser = await User.register(newUser, password);
+        req.flash("success", "Welcome to Yelpcamp");
+        res.redirect("/campgrounds");
+    } catch (error) {
+        req.flash("error", error.message);
+        res.redirect("/signup");
+    }
 }))
 
+router.get("/login", (req, res) => {
+    res.render("auth/login")
+})
+
+router.post("/login", passport.authenticate("local", { failureFlash: true, failureRedirect: "/login" }), async (req, res) => {
+    req.flash("success", "Welcome back!");
+    res.redirect("/campgrounds")
+})
 module.exports = router;
